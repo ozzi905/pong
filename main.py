7f85a8d3fac24d96ba5397e0d5e9187ca45bc55c
Ozzi905 <65067916+ozzi905@users.noreply.github.com>
2022-07-21 22:52:33 +0100

    added paddle image and test rectangle is now on screen

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-def main():
-    pass
+import pygame as pg
+import sys
+from pygame.locals import *
+ 
+# Initialize program
+pg.init()
+ 
+# Assign FPS a value
+FPS = 60
+FramePerSec = pg.time.Clock()
+ 
+# Setting up color objects
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+MAGENTA = (255,0,255)
+CYAN = (0,255,255)
+WHITE = (255,255,255)
 
-main()
+# Setup a 300x300 pixel display with caption
+window = pg.display.set_mode((800,800))
+window.fill(BLUE)
+pg.display.set_caption("Pong")
+
+test_surface = pg.Surface((300,300))
+
+class Paddle():
+    def __init__(self, x, y, vertical):
+        self.x = x
+        self.y = y
+        if(vertical == True): self.surface = pg.Surface((80, 20))
+        else: self.surface = pg.Surface((20, 80))
+
+while True:
+    pg.display.update()
+    for event in pg.event.get():
+        if event.type == QUIT:
+            pg.quit()
+            sys.exit()
+    test_surface.fill(CYAN)
+    window.blit(test_surface, (0,0))
+    
+   
+    FramePerSec.tick(FPS)
