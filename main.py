bc3952c93b411c56ddae7c831798b5e6f433b7cd
Ozzi905 <65067916+ozzi905@users.noreply.github.com>
2022-07-22 00:52:37 +0100

    added ball functionality ball will reflect off of sides and paddle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 from pygame.locals import *
+import random
  
 # Initialize program
 pg.init()
@@ -19,8 +20,11 @@ MAGENTA = (255,0,255)
 CYAN = (0,255,255)
 WHITE = (255,255,255)
 
+width = 1280
+height = 720
+
 # Setup a 300x300 pixel display with caption
-window = pg.display.set_mode((1280,720))
+window = pg.display.set_mode((width,height))
 window.fill(BLUE)
 pg.display.set_caption("Pong")
 
@@ -28,6 +32,14 @@ bg = pg.image.load("images/background.png").convert()
 
 paddle_surf = pg.image.load("images/paddle.png").convert()
 paddle_rect = paddle_surf.get_rect(midleft = (0, 360))
+
+ball_vel_y = random.randint(-10, 10) 
+ball_vel_x = 10
+
+ball_surf = pg.image.load("images/ball.png")
+ball_rect = ball_surf.get_rect()
+ball_rect.center = (width/2, height/2)
+ball_dir = "l"
 
 while True:
     pg.display.update()
@@ -43,6 +55,18 @@ while True:
 
     window.blit(paddle_surf, paddle_rect)
 
+    window.blit(ball_surf, ball_rect)
+
+    ball_rect.x += ball_vel_x
+    ball_rect.y += ball_vel_y
+
+    if(ball_rect.right >= width):
+        ball_vel_x = -ball_vel_x
+        ball_dir = "l"
+
+    if(ball_rect.bottom >= height or ball_rect.top <= 0): ball_vel_y = -ball_vel_y
     
-   
+    if(paddle_rect.colliderect(ball_rect) and ball_dir == "l"):
+        ball_vel_x = -ball_vel_x
+        ball_dir = "r"
     FramePerSec.tick(FPS)
